306bf5a9114cb38a3c5c11ec25e0030ba3ba5747
baole123123 <lechibaovlog@gmail.com>
2023-06-05 13:36:46 +0700

    baitap

---
+++ quản_lí_nhân_sự/process.js
@@ -0,0 +1,14 @@
+function addarray(params) {
+    var inputposition = document.getElementById('position').value;
+    var inputname = document.getElementById('name').value;
+    var inputyear = document.getElementById('year').value;
+    var inputwork = document.getElementById('work').value;
+    let list_student = array();
+}
+document.getElementById('position');
+document.getElementById('name');
+document.getElementById('year');
+document.getElementById('work');
+
+
+
